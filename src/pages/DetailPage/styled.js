5ad2b5565c8ab1516417501d23aae95f9a577586
Gazua300 <franfidinena@gmail.com>
2021-08-26 17:36:04 -0300

    Entrega do quarto dia de projeto

--- src/pages/DetailPage/styled.js
+++ src/pages/DetailPage/styled.js
@@ -5,19 +5,37 @@ import PokeBall from '../../img/pokeBall.png'
 export const Container = styled.div`
 	display: flex;
 `
-export const Images = styled.div`
-	display: flex;
-	flex-direction: column;	
-` 
 export const Figure = styled.img`
 	width: 20vw;		
 `
 export const Power = styled.div`	
 	background-color: lightgray;	
 	margin: 20px;
-	width: 25vw;
+	width: 23vw;
 	padding: 20px;
 `
 export const Text = styled.div`
 	line-height: 7vh;
+	padding: 10px;
 `
+export const Types = styled.div`	
+	background-color: lightgray;	
+	margin: 20px;
+	width: 23vw;	
+	height: 15vh;
+	padding: 5px;		
+`
+export const Moves = styled.div`
+	background-color: lightgray;	
+	margin: 20px;
+	width: 23vw;	
+	height: 55vh;
+	padding: 5px;
+`
+export const MovesAndTypes = styled.div`
+	/*este styled apesar de conter
+	nenhuma iformação foi necessário
+	para manter as duas ultimas divs
+	em posições de coluna*/
+`
+
